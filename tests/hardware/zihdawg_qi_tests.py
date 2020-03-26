b7eaa64365aed143358ceae7b5efade977ff34fd
ruuddejong <r.h.j.dejong@tudelft.nl>
2020-03-26 17:36:44 +0100

    Copied zihdawg.py to zihdawg_qi.py Copied zihdawg_tests.py to zihdawg_qi_tests.py

---
+++ tests/hardware/zihdawg_qi_tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from qupulse.hardware.awgs.zihdawg import HDAWGRepresentation, HDAWGChannelGroup, HDAWGWaveManager
+
+
+class HDAWGRepresentation(unittest.TestCase):
+    def test_init(self):
+        raise NotImplementedError()
+
+
+class HDAWGChannelGroupTests(unittest.TestCase):
+    def test_init(self):
+        raise NotImplementedError()
+
+
+class HDAWGWaveManagerTests(unittest.TestCase):
+    def test_init(self):
+        raise NotImplementedError()
